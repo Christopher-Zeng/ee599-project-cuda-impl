7701cff9f6ca8417ad7ead6cf7f9c3b71c483ec5
Christopher Zeng <ziyizeng@discovery1.usc.edu>
2021-11-26 13:46:35 -0800

    Added the skeleton file for trans-conv.

---
+++ src/trans-conv.cu
@@ -0,0 +1,14 @@
+bool gemm(float *opera, float *operb, float *operc, int H, int W, int K);
+bool gema(float *opera, float *operb, float *operc, int H, int W);
+
+/*
+    input: the input tensor to be convolved. Should be input[H][W][C] serialized array.
+    kernel: the kernel tensor. Should be kernel[C][M][K][K] serialized array. 
+    patch: the result patch tensor. Should be patch [H][W][M][K][K] serialized array.
+*/
+bool trans_conv(float *input, float *kernel, float *patch, int H, int W, int C, int M, int K);
+/*
+    patch: the patch tensor to be merged back together. Should be patch [H][W][M][K][K] serialized array.
+    output: the result output tensor. Should be result[M][H][W] serialized array. 
+*/
+bool shift_add(float *patch, float *output, int H, int W, int M, int K)
